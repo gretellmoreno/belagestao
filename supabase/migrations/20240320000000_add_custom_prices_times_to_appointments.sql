43af93585c49ce1950030f8fac6a2288e795a862
gretellmoreno <gretellmoreno43@gmail.com>
2025-06-17 21:42:47 -0300

    top

--- supabase/migrations/20240320000000_add_custom_prices_times_to_appointments.sql
+++
@@ -1,4 +0,0 @@
--- Adiciona os campos custom_prices e custom_times à tabela appointments
-ALTER TABLE appointments
-ADD COLUMN IF NOT EXISTS custom_prices jsonb DEFAULT '{}'::jsonb,
-ADD COLUMN IF NOT EXISTS custom_times jsonb DEFAULT '{}'::jsonb; 
--- supabase/migrations/20240326000000_add_cash_closing_id_to_employee_advances.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar coluna cash_closing_id à tabela employee_advances
-ALTER TABLE employee_advances
-ADD COLUMN cash_closing_id UUID REFERENCES cash_closings(id) ON DELETE SET NULL;
-
--- Criar índice para melhor performance
-CREATE INDEX employee_advances_cash_closing_id_idx ON employee_advances(cash_closing_id); 
